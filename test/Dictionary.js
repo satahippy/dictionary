44a83d601e3db37361edcc05717c87ba33ecb448
satahippy <satahippy@gmail.com>
2015-11-09 23:55:09 +0300

    more tests

--- test/Dictionary.js
+++ test/Dictionary.js
@@ -19,4 +19,15 @@ describe('Dictionary', function () {
 		assert.equal(dic.get('test'), 'Another Test Translation');
 		assert.equal(dic.get('test2'), 'Test Translation 2');
 	});
+
+	it('should create with empty dictionary', function () {
+		var dic = new Dictionary();
+		dic.merge({test: 'Test Translation'});
+		assert.equal(dic.get('test'), 'Test Translation');
+	});
+
+	it('should return undefined if word is not found', function () {
+		var dic = new Dictionary();
+		assert.equal(dic.get('test'), undefined);
+	});
 });
